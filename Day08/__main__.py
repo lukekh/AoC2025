cbc8e3b2c72421c13bad6b51488773e47caa5f85
Luke Keating Hughes <luke.keating.hughes@altra.cloud>
2025-12-10 10:57:42 +1030

    remove redundant adjacency matrix class

--- Day08/__main__.py
+++ Day08/__main__.py
@@ -7,7 +7,7 @@ from typing import Self
 import networkx as nx
 
 # Constants
-input_path = Path(__file__).parent / 'Day08.in'
+input_path = Path(__file__).parent / "Day08.in"
 LIMIT: int = 1000
 
 
@@ -36,48 +36,35 @@ def parse(path: Path):
     return [JunctionBox.parse(row) for row in path.read_text().strip().splitlines()]
 
 
-class AdjacencyMatrix:
-    """This adjacency matrix should speed things up"""
-    def __init__(self):
-        self.dict: dict[tuple[JunctionBox, JunctionBox], float] = {}
-    
-    def __getitem__(self, item: tuple[JunctionBox, JunctionBox]):
-        # Calculate dist if first time seeing JunctionBox pair
-        if item not in self.dict:
-            d1, d2 = item
-            self.dict[item] = d1.dist2(d2)
-            self.dict[(d2,d1)] = self.dict[item]
-        # Return pre-calculated dist
-        return self.dict[item]
-
-ADJACENCY = AdjacencyMatrix()
-
 def solution_generator(data: list[JunctionBox], limit: int = LIMIT):
     """Solution to part one"""
     # All pairs possible sorted from closest to furthest away
-    inv_function = sorted(((ADJACENCY[j1,j2], (j1,j2)) for i, j1 in enumerate(data) for j2 in data[i+1:]), key=lambda t: t[0])
+    dist2_ranked_pairs = sorted(
+        ((j1.dist2(j2), (j1, j2)) for i, j1 in enumerate(data) for j2 in data[i+1:]),
+        key=lambda t: t[0],
+    )
 
     graph: nx.Graph[JunctionBox] = nx.Graph()
     remaining_nodes = set(data)
 
     # Determine edges
-    for _, (j1, j2) in inv_function[:limit]:
-        graph.add_edge(j1,j2)
+    for _, (j1, j2) in dist2_ranked_pairs[:limit]:
+        graph.add_edge(j1, j2)
         remaining_nodes -= {j1, j2}
-    
+
     # Yield solution to part one
     yield prod(
         sorted(map(len, nx.connected_components(graph)))[-3:]
     )
 
     # Continue on until it is one connected component
-    for _, (j1, j2) in inv_function[limit:]:
-        graph.add_edge(j1,j2)
+    for _, (j1, j2) in dist2_ranked_pairs[limit:]:
+        graph.add_edge(j1, j2)
         remaining_nodes -= {j1, j2}
         if not remaining_nodes and nx.is_connected(graph):
             # Yield solution to part two
             yield j1.x * j2.x
-    
+
     raise ValueError("graph was never connected")
 
 
@@ -93,6 +80,5 @@ def main():
     print(f"Part 2: {next(gen)}")
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
